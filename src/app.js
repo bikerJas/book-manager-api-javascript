ec0ab970bca00cdf045d94c9bd1d1a1061daba04
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-30 19:17:00 +0000

    Enabled book-manager-api with Swagger

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
-const express = require('express');
+// Swagger JS Doc and Swagger UI for Express
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+const express = require("express");
 const app = express();
-const routes = require('./routes');
+const routes = require("./routes");
+
+// Swagger Configuration
+const swaggerConfiguration = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: ' Minimalist Book Manager API',
+            version: '0.1.0',
+            description: 
+            'Need a helping hand to manage your books ?' +
+            'Well, you\'re in luck! This API will let you manage your books with ease! ',
+        },
+    },
+    apis: ['src/routes/index.js'],
+};
+
+// Initialize Swagger-jsdoc -> returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(swaggerConfiguration);
+
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.use(express.json());
 
-app.use('/api/v1', routes);
+app.use("/api/v1", routes);
 
 module.exports = app;
