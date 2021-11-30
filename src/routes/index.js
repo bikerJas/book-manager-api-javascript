9fbbabf7d7e74e19854b95df1d08005cb0ade938
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-11-30 19:37:50 +0000

    Annotated /books endpoint

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,15 @@ const express = require('express');
 const {booksController} = require('../controllers');
 
 const router = new express.Router();
+/**
+ * @swagger
+ * /api/v1/books:
+ *  get:
+ *    summary: Get all books
+ *    responses:
+ *      200:
+ *          description: An array of books
+ */
 router.get('/books', booksController.getBooks);
 router.get('/books/:bookId', booksController.getBook);
 router.post('/books', booksController.saveBook);
