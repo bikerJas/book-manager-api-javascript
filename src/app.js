ab754827defd83ea9671317a8118b50d11a3508c
bikerJas <18085914+bikerJas@users.noreply.github.com>
2021-12-01 21:33:11 +0000

    Annotated /single book endpoint

--- src/app.js
+++ src/app.js
@@ -1,29 +1,30 @@
-// Swagger JS Doc and Swagger UI for Express
-const swaggerJSDoc = require("swagger-jsdoc");
-const swaggerUI = require("swagger-ui-express");
 const express = require("express");
 const app = express();
 const routes = require("./routes");
 
+// Swagger JS Doc and Swagger UI for Express
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
+
 // Swagger Configuration
 const swaggerConfiguration = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: ' Minimalist Book Manager API',
-            version: '0.1.0',
-            description: 
-            'Need a helping hand to manage your books ?' +
-            'Well, you\'re in luck! This API will let you manage your books with ease! ',
-        },
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: " Minimalist Book Manager API",
+      version: "0.1.0",
+      description:
+        "Need a helping hand to manage your books ?" +
+        "Well, you're in luck! This API will let you manage your books with ease! ",
     },
-    apis: ['src/routes/index.js'],
+  },
+  apis: ["src/routes/index.js"],
 };
 
 // Initialize Swagger-jsdoc -> returns validated swagger spec in json format
-const swaggerSpec = swaggerJSDoc(swaggerConfiguration);
+const swaggerDocs = swaggerJSDoc(swaggerConfiguration);
 
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.use(express.json());
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,26 @@
-const express = require('express');
-const {booksController} = require('../controllers');
+const express = require("express");
+const { booksController } = require("../controllers");
 
 const router = new express.Router();
+
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    Book:
+ *      type: object
+ *      required:
+ *          - title
+ *          - author
+ *      properties:
+ *       id:
+ *          type: integer
+ *       title:
+ *          type: string
+ *       author:
+ *          type: string
+ */
+
 /**
  * @swagger
  * /api/v1/books:
@@ -10,15 +29,42 @@ const router = new express.Router();
  *    responses:
  *      200:
  *          description: An array of books
+ *      content:
+ *        application/json:
+ *         schema:
+ *          type: array
+ *          items:
+ *            $ref: '#/components/schemas/Book'
+
  */
-router.get('/books', booksController.getBooks);
-router.get('/books/:bookId', booksController.getBook);
-router.post('/books', booksController.saveBook);
+router.get("/books", booksController.getBooks);
+
+/**
+ * @swagger
+ * /api/v1/books/{bookId}:
+ *   get:
+ *     summary: Get a book by bookId
+ *     parameters:
+ *       - in: path
+ *         schema:
+ *           type: integer
+ *         name: bookId
+ *         description: ID of book
+ *         required: true
+ *     responses:
+ *       '200':
+ *          description: Numeric ID of the book to get
+ *       '404':
+ *          description: book cannot be found
+ */
+router.get("/books/:bookId", booksController.getBook);
+
+router.post("/books", booksController.saveBook);
 
 // User Story 4 - Update Book By Id Solution
-router.put('/books/:bookId', booksController.updateBook);
+router.put("/books/:bookId", booksController.updateBook);
 
 // User Story: As a user, I want to use the Book Manager API to delete a book using its ID
-router.delete('/books/:bookId', booksController.deleteBook);
+router.delete("/books/:bookId", booksController.deleteBook);
 
 module.exports = router;
